de759758e4ff4ec3d45d3c3e186edc5c8977600a
jfgreffier <jfgreffier@users.noreply.github.com>
2024-07-02 20:08:22 +0200

    feat: e2e schematic

---
+++ src/e2e/index.ts
@@ -0,0 +1,19 @@
+import { classify, dasherize } from '@angular-devkit/core/src/utils/strings';
+import { Rule, SchematicContext, Tree, apply, applyTemplates, chain, mergeWith, move, url } from '@angular-devkit/schematics';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export default function e2e(_options: any): Rule {
+  const name = _options.name;
+  return (tree: Tree, _context: SchematicContext) => {
+    const templateSource = apply(url('./files'), [
+      applyTemplates({classify, name, dasherize}),
+      move('e2e')
+    ]);
+
+    const rule = chain([mergeWith(templateSource)]);
+
+    return rule(tree, _context);
+  };
+}

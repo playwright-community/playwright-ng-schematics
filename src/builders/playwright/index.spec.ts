bc6bda57d9c5e8a7ab5822757463fc05feb8f28d
jfgreffier <jfgreffier@users.noreply.github.com>
2024-09-17 22:08:46 +0200

    test: init builder tests

---
+++ src/builders/playwright/index.spec.ts
@@ -0,0 +1,33 @@
+import { spawnSync } from 'node:child_process';
+import { join } from 'node:path';
+import { Architect } from '@angular-devkit/architect';
+import { TestingArchitectHost } from '@angular-devkit/architect/testing';
+import { schema } from '@angular-devkit/core';
+
+jest.mock('node:child_process');
+
+describe('Playwright builder', () => {
+  let architect: Architect;
+
+  beforeEach(async () => {
+    const architectHost = new TestingArchitectHost();
+    architect = new Architect(architectHost);
+    await architectHost.addBuilderFromPackage(join(__dirname, '../../..'));
+  });
+
+  it('should spawn testing process', async () => {
+    const run = await architect.scheduleBuilder(
+      'playwright-ng-schematics:playwright',
+      {},
+    );
+
+    await run.result;
+    await run.stop();
+
+    expect(spawnSync).toHaveBeenCalledWith(
+      'npx playwright test',
+      [],
+      expect.anything(),
+    );
+  });
+});
